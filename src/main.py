e3c2a463ab4ae957859159bf7fd28fe536854f11
Ricardo Estrada <ricardo.ares1989@gmail.com>
2020-06-09 11:05:17 -0500

    Finding the encoded message

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
+import re
+
 # Resolve the problem!!
 
 
 def run():
     # Start coding here
+    with open('./encoded.txt', 'r', encoding='utf-8') as f:
+        text = f.readlines()
+    pattern = re.compile(r'[^a-z\s]*')
+    result = re.sub(pattern,'', text[0])
+    print(result)
+
 
 
 if __name__ == '__main__':
